461db92f76c3e46d6a45960b68fb5f232be8eb6c
elKryptos <154229030+elKryptos@users.noreply.github.com>
2024-06-08 12:42:37 +0200

    union

--- src/app/cognito.service.ts
+++ src/app/cognito.service.ts
@@ -1,9 +1,70 @@
 import { Injectable } from '@angular/core';
+import { AuthenticationDetails, CognitoUser, CognitoUserAttribute, CognitoUserPool } from 'amazon-cognito-identity-js';
+import { environment } from './environment/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CognitoService {
 
+
   constructor() { }
+
+  cognitoPool = new CognitoUserPool(environment.congnito)
+
+  userSignUp(username: string, password: string, email: string, name: string, surname: string): Promise<string>{
+    
+    let mailAttribute = new CognitoUserAttribute({
+      Name: "email", 
+      Value: email
+    })
+
+    let nameAttribute = new CognitoUserAttribute({
+      Name: "given_name", 
+      Value: name
+    })
+
+    let surnameAttribute = new CognitoUserAttribute({
+      Name: "family_name", 
+      Value: surname
+    })
+
+    return new Promise((resolve, reject) => {
+      this.cognitoPool.signUp(username, password, [mailAttribute, nameAttribute, surnameAttribute], [], (err, res)=>{
+        if (err) reject(err.message)
+        else resolve("OK")
+      })
+    })
+  }
+
+  userSingIn(username: string, password: string): Promise<string>{
+    let user = new CognitoUser({
+      Pool: this.cognitoPool,
+      Username: username
+    })
+
+    let details = new AuthenticationDetails({ Username: username, Password: password })
+
+    return new Promise ((resolve, reject)=> {
+      user.authenticateUser(details, {
+        onSuccess: () => { resolve("OK")},
+        onFailure: (err) => { reject(err.message)}
+      })
+    })
+  }
+
+  confirm(username: string, code: string){
+    let user = new CognitoUser({
+      Pool: this.cognitoPool,
+      Username: username
+    })
+
+    return new Promise((resolve, reject)=>{
+      user.confirmRegistration(code, false, (err, res) => {
+        if(err) reject(err.message)
+        else resolve("OK")
+      })
+    })
+  }
+
 }
---
+++ src/app/environment/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    congnito: {
+        UserPoolId: "eu-west-3_UzPXndP9I",
+        ClientId: "3qn0pu3i5hg2pmmna10lr9tlep"
+    }
+}
